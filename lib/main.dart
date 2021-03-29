cc88f8d4216fc22982771cef0c9e2c4ef79177ae
Mak Yen Wei <shaonmak@gmail.com>
2021-03-30 03:59:01 +0800

    feat: new scaffold color
    Co-authored-by: sinyin-ong <sinyin-ong@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        scaffoldBackgroundColor: const Color(0xEAEAEAEF),
+        scaffoldBackgroundColor: const Color(0xFAFFFFFF),
         primarySwatch: Colors.blue,
       ),
       initialRoute: '/home',
