4a3ecd0f922f42fcf2ab698e198f6b7dfb893314
Shaunmak1214 <shaonmak@gmail.com>
2021-08-05 10:11:30 +0800

    feat: changes made on dashboard-done

---
+++ lib/apis/SpaceNews.dart
@@ -0,0 +1,159 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SpaceNews {
+  final int id;
+  final String title;
+  final String url;
+  final String imageUrl;
+  final String newsSite;
+  final String summary;
+  final String publishedAt;
+
+  SpaceNews(
+      {this.id = 1,
+      this.title = "",
+      this.url = "",
+      this.imageUrl = "",
+      this.newsSite = "",
+      this.summary = "",
+      this.publishedAt = ""});
+
+  factory SpaceNews.fromJson(Map<String, dynamic> json) {
+    return SpaceNews(
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      imageUrl: json['imageUrl'],
+      newsSite: json['newsSite'],
+      summary: json['summary'],
+      publishedAt: json['publishedAt'],
+    );
+  }
+}
+
+class SpaceNewsListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<SpaceNews>>(
+      future: _fetchNews(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<SpaceNews>? data = snapshot.data;
+          return _newsListView(data);
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  Future<List<SpaceNews>> _fetchNews() async {
+    var url = new Uri.https('api.spaceflightnewsapi.net', '/v3/articles');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      print(response.body);
+      return jsonResponse.map((news) => new SpaceNews.fromJson(news)).toList();
+    } else {
+      throw Exception('Failed to load jobs from API');
+    }
+  }
+
+  ListView _newsListView(data) {
+    return ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return newsCard(data[index].title, data[index].url,
+              data[index].imageUrl, data[index].newsSite, data[index].summary);
+        });
+  }
+
+  Container newsCard(String title, String url, String imageUrl, String newsSite,
+          String summary) =>
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          color: Color.fromRGBO(255, 255, 255, 1.0),
+        ),
+        margin: const EdgeInsets.only(bottom: 10.0),
+        child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 4,
+                    child: Container(
+                      width: 150.0,
+                      height: 125.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.cover, image: NetworkImage(imageUrl)),
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                        color: Color.fromRGBO(245, 245, 245, 1.0),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 6,
+                    child: Padding(
+                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(title,
+                                  style: TextStyle(
+                                      fontSize: 14.0,
+                                      fontFamily: 'Montserrat',
+                                      fontWeight: FontWeight.bold)),
+                              SizedBox(
+                                height: 45.0,
+                              ),
+                              Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.link_outlined,
+                                            size: 20.0,
+                                            semanticLabel: 'Link',
+                                          ),
+                                          Padding(
+                                              padding:
+                                                  EdgeInsets.only(left: 10.0)),
+                                          Text(newsSite),
+                                        ]),
+                                    Padding(
+                                        padding: EdgeInsets.only(left: 10.0)),
+                                    Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Icon(
+                                            Icons.timelapse_outlined,
+                                            size: 20.0,
+                                            semanticLabel: 'Link',
+                                          ),
+                                          Padding(
+                                              padding:
+                                                  EdgeInsets.only(left: 10.0)),
+                                          Text("1h"),
+                                        ])
+                                  ])
+                            ])),
+                  )
+                ])),
+      );
+}
